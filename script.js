a401d598526b175c0666ccbda60a270bdd79a0f7
AntoniaNicole <antonia.cretoiu@gmail.com>
2023-02-14 18:21:30 +0100

    Play/Pause Button + Links

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function myFunction() {
     document.getElementById("play/pause-button").innerHTML = "⏸️"; 
 }
+var x = "play"
+function getinnerHTML() {
+if(x=== "play") {
+    document
+    x= "pause"
+    document.getElementById("play/pause-button").innerHTML = "▶️"
+}else{
+    x="play"
+    document.getElementById("play/pause-button").innerHTML = "⏸️"
+}
+}
