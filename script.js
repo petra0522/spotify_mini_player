7b5499fe9b288b852a7081e8a346836f9f195f25
Peng, Siyuan <Siyuan.Peng@adesso.de>
2023-02-16 10:14:20 +0100

    added a function for sending request

--- script.js
+++ script.js
@@ -17,5 +17,24 @@ function log() {
   console.log("Klick");
 }
 function accessToken() {
-     localStorage.setItem("token", document.getElementById("token").value );
+  localStorage.setItem("token", document.getElementById("token").value);
+}
+
+function request(url, method, body) {
+  // TODO: read the token from localStorage und save in a variable named token
+
+  const headers = new Headers();
+  headers.append("Content-Type", "application/json");
+  headers.append("Authorization", "Bearer " + token);
+  const data = {
+    method: method,
+    headers: headers,
+    body: JSON.stringify(body),
+  };
+  return fetch(url, data).then((response) => {
+    if (response.status === 401) {
+      alert("Token is expired! Please enter a new token.");
+    }
+    return response.json();
+  });
 }
